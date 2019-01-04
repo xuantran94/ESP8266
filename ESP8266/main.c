bc0e84e92ec2acc8d62c34ed2ee08a320bdb1c6f
Tran Xuan <thanhxuantran.1994@gmail.com>
2019-01-04 21:40:46 +0700

    Initial Commit

---
+++ ESP8266/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ESP8266
+//
+//  Created by Tran Xuan on 1/4/19.
+//  Copyright © 2019 Tran Xuan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
